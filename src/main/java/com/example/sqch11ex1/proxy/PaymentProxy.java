6012852c6cbd436523924d7df245f9029ee8fc02
Artem Balan <a.s.balan96@gmail.com>
2024-02-03 21:38:42 +0200

    create OpenFeign REST endpoint

---
+++ src/main/java/com/example/sqch11ex1/proxy/PaymentProxy.java
@@ -0,0 +1,16 @@
+package com.example.sqch11ex1.proxy;
+
+import com.example.sqch11ex1.model.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "payments", url = "${name.service.url}")
+public interface PaymentProxy {
+
+    @PostMapping("/payment")
+    Payment createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment);
+}
